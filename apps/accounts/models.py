df1a142b762418163393a5b0ebfc86da911fe534
Ronil Rufo <ronil.rufo@gmail.com>
2021-12-11 18:37:15 +0800

    Used AbstractBaseUser in CustomUser

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,19 +1,31 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
+from .managers import CustomUserManager
 
-class CustomUser(AbstractUser):
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     """
     Custom User model using email as username.
     """
 
-    username = None
     email = models.EmailField(_("Email Address"), unique=True)
+    first_name = models.CharField(_("first name"), max_length=30)
+    last_name = models.CharField(_("last name"), max_length=30)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManager()
 
     class Meta:
         verbose_name = _("Custom User")
         verbose_name_plural = _("Custom Users")
+
+    def __str__(self) -> str:
+        return self.email
