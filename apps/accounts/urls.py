43ac0c20f95f4c0347af110ef571028bd84adced
Ronil Rufo <ronil.rufo@gmail.com>
2021-12-12 14:04:07 +0800

    Implemented Registration view

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,10 +4,11 @@ URL patterns in accounts app
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from .views import Login
+from .views import Login, Registration
 
 app_name = "accounts"
 urlpatterns = [
     path("login/", Login.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", Registration.as_view(), name="register"),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
+from django.views.generic import FormView
 
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 
 
 class Login(LoginView):
@@ -17,3 +20,18 @@ class Login(LoginView):
         Returns the redirect URL upon successful login.
         """
         return reverse_lazy("posts:post-list")
+
+
+class Registration(FormView):
+    """
+    Allows the user to register an account.
+    """
+
+    template_name = "accounts/register.html"
+    form_class = RegistrationForm
+    success_url = reverse_lazy("posts:post-list")
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return HttpResponseRedirect(self.get_success_url())
