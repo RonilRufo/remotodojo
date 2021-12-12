0ef36a4110cd832b23068ca65d2e2c161bd5043d
Ronil Rufo <ronil.rufo@gmail.com>
2021-12-12 14:03:31 +0800

    added RegistrationForm

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -33,3 +33,21 @@ class LoginForm(AuthenticationForm):
         self.fields["username"].widget.attrs["placeholder"] = "Enter Email Address..."
         self.fields["username"].widget.attrs["aria-describedby"] = "emailHelp"
         self.fields["password"].widget.attrs["placeholder"] = "Password"
+
+
+class RegistrationForm(UserCreationForm):
+    """
+    Form to be used in registration.
+    """
+
+    class Meta:
+        model = CustomUser
+        fields = ("email", "first_name", "last_name")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs["class"] = "form-control form-control-user"
+            self.fields[field].widget.attrs["placeholder"] = self.fields[
+                field
+            ].label.title()
